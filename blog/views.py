d93cbcc45872d26602c8bc58f15a0f41b57327ce
Josiah Anderson <riddlerforever@live.com>
2022-05-20 02:45:54 -0500

    I added a subdir in the templates dir; modified urls and views

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. You're at the blog index.")
 
 def view_by_year(request):
     return HttpResponse("Hello, this is all of the years' blogs.")
+
+
