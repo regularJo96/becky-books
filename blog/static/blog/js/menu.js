f7bbf5a13e90f163fe70c838c2483fda247ae7e2
Josiah Anderson <riddlerforever@live.com>
2022-05-21 17:58:33 -0500

    added background images to home page; added grid and flex boxes to arrange home page

---
+++ blog/static/blog/js/menu.js
@@ -0,0 +1,13 @@
+// function expand(){
+//     document.getElementById("media").className = "media-links-active";
+//     document.getElementById("bookshelf").className = "bookshelf-active";
+//     document.getElementById("academics").className = "academics-active";
+//     document.getElementById("blog").className = "blog-active";
+// }
+
+// function reduce(){
+//     document.getElementById("media").className = "media-links";
+//     document.getElementById("bookshelf").className = "bookshelf";
+//     document.getElementById("academics").className = "academics";
+//     document.getElementById("blog").className = "blog";
+// }
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
-
-def view_by_year(request):
-    return HttpResponse("Hello, this is all of the years' blogs.")
-
-
+    return render(request, 'blog/home.html', {})
