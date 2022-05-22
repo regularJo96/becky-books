13da4ebf32c28402a66609b57898e5dd4ada9549
Josiah Anderson <riddlerforever@live.com>
2022-05-21 21:49:07 -0500

    added links to academia, blog and shelf; changed the background to home page

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'blog/home.html', {})
+
+def blog(request):
+    return render(request, 'blog/blog.html', {})
+
+def bookshelf(request):
+    return render(request, 'blog/bookshelf.html', {})
+
+def academia(request):
+    return render(request, 'blog/academia.html', {})
