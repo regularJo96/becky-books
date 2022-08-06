b7b8904ab47261f2a905c76c2966174be7883be4
Josiah Anderson <riddlerforever@live.com>
2022-08-06 06:08:47 -0500

    this branch is now a barebones html css and javascript branch just for showing

--- blog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Blog, Book
-# Register your models here.
-admin.site.register(Blog)
-admin.site.register(Book)
--- bookshelf/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('bookshelf/', views.bookshelf, name='bookshelf'),
-    path('add-book/', views.add, name='add_book')
-]
